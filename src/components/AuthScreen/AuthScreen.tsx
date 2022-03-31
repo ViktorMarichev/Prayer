697e9387c3755dbe460fb6631062ed51e0cd910a
=Viktor <maricevviktor2@gmail.com>
2022-03-31 18:41:21 +0600

    feat:(AuthScreen.tsx) authorization added

--- src/components/AuthScreen/AuthScreen.tsx
+++ src/components/AuthScreen/AuthScreen.tsx
@@ -1,10 +1,13 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement, useEffect} from 'react';
 import {TextInput} from 'react-native';
 import styled from 'styled-components/native';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import RootStackParamList from 'src/types/RootStackParamList';
 import {useForm, Controller, SubmitHandler} from 'react-hook-form';
 import {ErrorMessage} from '@hookform/error-message';
+import {useAppDispatch, useAppSelector} from '../../redux/store';
+import {UserSelectors} from '../../redux/User/index';
+import {login} from '../../redux/User/index';
 type AuthorizationProps = NativeStackScreenProps<
   RootStackParamList,
   'Authorization'
@@ -15,7 +18,13 @@ const AuthScreen: React.FC<AuthorizationProps> = ({navigation}) => {
     email: string;
     password: string;
   }
-
+  const dispatch = useAppDispatch();
+  const userData = useAppSelector(state => UserSelectors.userData(state));
+  useEffect(() => {
+    if (userData.token) {
+      navigation.navigate('Desks');
+    }
+  }, [userData]);
   const {
     control,
     handleSubmit,
@@ -28,7 +37,7 @@ const AuthScreen: React.FC<AuthorizationProps> = ({navigation}) => {
   });
   const onSubmit: SubmitHandler<IFormInputs> = data => {
     console.log(data);
-    navigation.navigate('Desks');
+    dispatch(login(data));
   };
   return (
     <AuthScreenWrapper>

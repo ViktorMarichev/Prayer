090f04f615c5e9bdcb549babc50eaa13feeb476b
=Viktor <maricevviktor2@gmail.com>
2022-04-01 12:28:11 +0600

    feat:saga prayer added

--- src/redux/sagas.ts
+++ src/redux/sagas.ts
@@ -1,11 +1,13 @@
 import {call, put, takeLatest, all} from 'redux-saga/effects';
 import {User} from './api/user';
 import {Columns} from './api/columns';
+import {Prayers} from './api/prayers';
 import {login} from './User/slice';
 import {getColumns} from './Columns/slice';
+import {getPrayers} from './Prayers/index';
 type signInAction = ReturnType<typeof login>;
 type getColumnsAction = ReturnType<typeof getColumns>;
-
+type getPrayersAction = ReturnType<typeof getPrayers>;
 function* signInWorker(action: signInAction) {
   const {email, password} = action.payload;
   const {success, failure} = login;
@@ -49,6 +51,27 @@ function* getColumnsWorker(action: getColumnsAction) {
   }
 }
 
+function* getPrayersWorker(action: getPrayersAction) {
+  const {token} = action.payload;
+
+  const {success, failure} = getColumns;
+  try {
+    // запросить токен, получить его, а затем попытаться запросить данные
+    const {data: prayersArray} = yield call(Prayers.getAll, {token});
+    // в случае успеха отдать данные редьюсеру
+    if (prayersArray.message) {
+      yield put(failure({message: prayersArray.message}));
+    } else {
+      yield put(success(prayersArray));
+    }
+  } catch (error) {
+    // ошибку можно тоже отдать редьюсеру через вызов failure
+    // или получить в компоненте
+    // или вообще написать функцию-обработчик, правящую миром ошибок
+    console.error(error);
+  }
+}
+
 export function* columnsWatcher() {
   yield takeLatest(getColumns.TRIGGER, getColumnsWorker);
 }
@@ -56,9 +79,12 @@ export function* columnsWatcher() {
 export function* userWatcher() {
   yield takeLatest(login.TRIGGER, signInWorker);
 }
+export function* prayersWatcher() {
+  yield takeLatest(getPrayers.TRIGGER, getPrayersWorker);
+}
 
 // notice how we now only export the rootSaga
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
-  yield all([userWatcher(), columnsWatcher()]);
+  yield all([userWatcher(), columnsWatcher(), prayersWatcher()]);
 }

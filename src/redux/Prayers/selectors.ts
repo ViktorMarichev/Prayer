7d947e0af7b4c2b9178b4697e884081ab5f42126
Viktor Maritchev <maricevviktor2@gmail.com>
2022-04-07 18:33:16 +0600

    feat:(Prayers/selectors.ts)' sort  prayers by title'

--- src/redux/Prayers/selectors.ts
+++ src/redux/Prayers/selectors.ts
@@ -5,26 +5,54 @@ const getAll = (state: RootState) => {
   return state.prayers;
 };
 const getCheckedPrayers = (state: RootState, columnId: number) => {
-  return state.prayers.filter((prayer: Prayer) => {
+  return state.prayers.prayersList.filter((prayer: Prayer) => {
     return prayer.checked && prayer.columnId === columnId;
-  });
+  }).sort((a, b) => {
+    let fa = a.title.toLowerCase(),
+      fb = b.title.toLowerCase();
+
+    if (fa < fb) {
+      return -1;
+    }
+    if (fa > fb) {
+      return 1;
+    }
+    return 0;
+  });;
 };
 const getNotCheckedPrayers = (state: RootState, columnId: number) => {
-  return state.prayers.filter((prayer: Prayer) => {
+  return state.prayers.prayersList.filter((prayer: Prayer) => {
     return !prayer.checked && prayer.columnId === columnId;
+  }).sort((a, b) => {
+    let fa = a.title.toLowerCase(),
+      fb = b.title.toLowerCase();
+
+    if (fa < fb) {
+      return -1;
+    }
+    if (fa > fb) {
+      return 1;
+    }
+    return 0;
   });
 };
 const filterCardsById = (state: RootState, columnId: number) => {
-  return state.prayers.filter((prayer: Prayer) => {
+  return state.prayers.prayersList.filter((prayer: Prayer) => {
     return prayer.columnId === columnId;
   });
 };
 const getPrayersByColumnId = createSelector(filterCardsById, prayers => {
   return prayers;
 });
+const getCheckedPrayersSelector = createSelector(getCheckedPrayers, prayers => {
+  return prayers
+});
+const getNotCheckedPrayersSelector = createSelector(getNotCheckedPrayers, prayers => {
+  return prayers
+});
 export default {
   getAll,
   getPrayersByColumnId,
-  getCheckedPrayers,
-  getNotCheckedPrayers,
+  getCheckedPrayers: getCheckedPrayersSelector,
+  getNotCheckedPrayers: getNotCheckedPrayersSelector,
 };

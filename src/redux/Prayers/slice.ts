e16701cec48a5a96510afc517f77d745172d723a
=Viktor <maricevviktor2@gmail.com>
2022-04-09 14:09:29 +0600

    fix(Prayers/slice.ts):'fix delete prayer reducer'

--- src/redux/Prayers/slice.ts
+++ src/redux/Prayers/slice.ts
@@ -1,25 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { createRoutine } from 'redux-saga-routines';
+import {createSlice} from '@reduxjs/toolkit';
+import {createRoutine} from 'redux-saga-routines';
 import Prayer from 'src/types/Prayer';
 export const getPrayers = createRoutine('prayers/getAll');
 export const createPrayer = createRoutine('prayers/create');
 export const deletePrayer = createRoutine('prayers/delete');
 export const updatePrayer = createRoutine('prayers/update');
 type PrayerType = {
-  prayersList: Array<Prayer>,
-  message: string | null,
-}
+  prayersList: Array<Prayer>;
+  message: string | null;
+};
 const PrayersSlice = createSlice({
   name: 'userSlice',
   initialState: {
     prayersList: [],
-    message: null
+    message: null,
   } as PrayerType,
   reducers: {
-    clearMessage: (state) => {
+    clearMessage: state => {
       state.message = null;
     },
-
   },
   extraReducers: {
     [getPrayers.SUCCESS]: (state, action) => {
@@ -27,16 +26,18 @@ const PrayersSlice = createSlice({
     },
     [getPrayers.FAILURE]: (state, action) => {
       console.log(action.payload.message);
-      return { ...state, message: action.payload.message };
+      return {...state, message: action.payload.message};
     },
     [createPrayer.SUCCESS]: (state, action) => {
-      return state.message = action.payload.message;
+      return (state.message = action.payload.message);
     },
     [deletePrayer.SUCCESS]: (state, action) => {
-      const newPrayerList = void (state.prayersList.filter((elem) => {
-        return elem.id != action.payload.id
-      }))
-      return { prayersList: newPrayerList, message: 'done', }
+      return {
+        prayersList: state.prayersList.filter(elem => {
+          return elem.id != action.payload.id;
+        }),
+        message: 'done',
+      };
     },
     [deletePrayer.FAILURE]: (state, action) => {
       state.message = action.payload.message;
@@ -44,21 +45,17 @@ const PrayersSlice = createSlice({
     [updatePrayer.SUCCESS]: (state, action) => {
       const newPrayer: Prayer = action.payload;
       return {
-        prayersList: state.prayersList.map((elem) => {
+        prayersList: state.prayersList.map(elem => {
           if (elem.id == newPrayer.id) {
-            return newPrayer
+            return newPrayer;
           }
           return elem;
-
-        })
-      }
-
-
+        }),
+      };
     },
     [updatePrayer.FAILURE]: (state, action) => {
-
       state.message = action.message;
-    }
+    },
   },
 });
 export const actions = PrayersSlice.actions;

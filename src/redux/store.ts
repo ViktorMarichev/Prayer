9aa4d3259a2006a1aae1832a2d461dad46a634a6
=Viktor <maricevviktor2@gmail.com>
2022-04-09 20:36:29 +0600

    feat:plug in redux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,6 +2,9 @@ import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import {TypedUseSelectorHook, useSelector, useDispatch} from 'react-redux';
 import UserReducer from './User/index';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import columnsReducer from './Columns/index';
 import PrayersSlice from './Prayers/index';
 import rootSaga from './sagas';
@@ -11,12 +14,19 @@ const rootReducer = combineReducers({
   columns: columnsReducer,
   prayers: PrayersSlice,
 });
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['user'],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(sagaMiddleware),
+  reducer: persistedReducer,
+  middleware: [sagaMiddleware],
 });
+
 sagaMiddleware.run(rootSaga);
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();

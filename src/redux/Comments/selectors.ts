608ef29af5a7f6e0cc7b62cbcdf0202fac1f086f
=Viktor <maricevviktor2@gmail.com>
2022-04-10 14:09:15 +0600

    feat:Comments selector added

--- src/redux/Comments/selectors.ts
+++ src/redux/Comments/selectors.ts
@@ -1,7 +1,31 @@
+import {createSelector} from '@reduxjs/toolkit';
 import {RootState} from '../store';
+import Comment from 'src/types/Comment';
 const getAll = (state: RootState) => {
   return state.comments;
 };
+const filterCommentsById = (state: RootState, prayerId: number) => {
+  return state.comments.commentsList
+    .filter((comment: Comment) => {
+      return comment.prayerId === prayerId;
+    })
+    .sort((a, b) => {
+      let fa = new Date(a.created),
+        fb = new Date(b.created);
+
+      if (fa < fb) {
+        return -1;
+      }
+      if (fa > fb) {
+        return 1;
+      }
+      return 0;
+    });
+};
+const getCommentsByPrayerId = createSelector(filterCommentsById, prayers => {
+  return prayers;
+});
 export default {
   getAll,
+  getCommentsByPrayerId,
 };
--- src/redux/Comments/slice.ts
+++ src/redux/Comments/slice.ts
@@ -1,11 +1,18 @@
 import {createRoutine} from 'redux-saga-routines';
 import {createSlice} from '@reduxjs/toolkit';
+import Comment from 'src/types/Comment';
 export const getComments = createRoutine('comments/GET_ALL');
+
+type CommentType = {
+  commentsList: Array<Comment>;
+  message: null | string;
+};
 const CommentSlice = createSlice({
   name: 'columnsSlice',
   initialState: {
     commentsList: [],
-  },
+    message: null,
+  } as CommentType,
   reducers: {},
   extraReducers: {
     [getComments.SUCCESS]: (state, action) => {

93bc212cb790dc5d6c3cf5ae6fa1e5f92abe361d
=Viktor <maricevviktor2@gmail.com>
2022-04-12 14:58:44 +0600

    fix:(DetailsScreen.tsx):'Fix button style'

--- src/assets/svg/Union.tsx
+++ src/assets/svg/Union.tsx
@@ -7,7 +7,7 @@ const SvgUnion = (props: SvgProps) => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M9 1a1 1 0 1 0-2 0v6H1a1 1 0 0 0 0 2h6v6a1 1 0 1 0 2 0V9h6a1 1 0 1 0 0-2H9V1Z"
-      fill="#72A8BC"
+      fill={props.color ? props.color : '#72A8BC'}
     />
   </Svg>
 );
